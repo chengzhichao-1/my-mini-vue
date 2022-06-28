57f9ce30b636cd3a0955630b36be4397b59c87c8
chengzhichao-1 <240087976@qq.com>
2022-06-28 10:21:52 +0800

    effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,42 @@
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn: () => void) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+let activeEffect;
+
+const targetMap = new WeakMap();
+
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  dep.forEach((effect) => {
+    effect.run();
+  });
+}
+
+export function effect(fn: () => void) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
--- src/reactivity/tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import { add } from "../index"
-
-it('should ', () => {
-  expect(add(1, 1)).toBe(2)
-});
