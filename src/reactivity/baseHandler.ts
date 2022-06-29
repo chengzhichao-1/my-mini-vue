b85de28f5ef7c403a998c402502b86c3cbc3ba48
chengzhichao-1 <240087976@qq.com>
2022-06-29 09:44:24 +0800

    实现isReadonly && isReactive

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
@@ -7,6 +8,12 @@ const readonlyGet = createGetter(true);
 function createGetter(isReadonly = false) {
   return function get(target, key, receiver) {
     const res = Reflect.get(target, key, receiver);
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    }
+    if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    }
     track(target, key);
     return res;
   };
