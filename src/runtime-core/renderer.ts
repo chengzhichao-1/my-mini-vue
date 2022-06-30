9841067b60da58d393916ba12c0557c4604f31a5
chengzhichao-1 <240087976@qq.com>
2022-06-30 18:33:42 +0800

    element初始化主流程

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -5,7 +6,39 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  } else if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+
+  if (vnode.props) {
+    for (const key in vnode.props) {
+      el.setAttribute(key, vnode.props[key]);
+    }
+  }
+
+  if (typeof vnode.children === "string") {
+    el.textContent = vnode.children;
+  } else if (Array.isArray(vnode.children)) {
+    mountChildren(vnode, el);
+  }
+
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
