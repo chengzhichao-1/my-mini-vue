cd81db1adb48b93dd4d31db74e45066d3fd7a4f6
chengzhichao-1 <240087976@qq.com>
2022-07-10 12:23:27 +0800

    编译模块之解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast"
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -21,27 +26,60 @@ function parseChildren(context) {
   const nodes: any = []
 
   let node
-  context.source = context.source.trim()
-  if (context.source.startsWith("{{")) {
+  let s = context.source
+  s = s.trim()
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] === "<") {
+    if (/[a-zA-Z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
 
   nodes.push(node)
   return nodes
 }
 
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+  // <div></div>
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if (type === TagType.End) return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length);
+}
+
 function parseInterpolation(context) {
   const openDelimiter = "{{"
   const closeDelimiter = "}}"
 
-  context.source = context.source.slice(openDelimiter.length)
-
+  advanceBy(context, openDelimiter.length)
+  
   const closeIndex = context.source.indexOf(closeDelimiter)
   const rawContentLength = closeIndex
   const rawContent = context.source.slice(0, rawContentLength)
   const content = rawContent.trim()
   
-  context.source = context.source.slice(rawContentLength + closeDelimiter.length)
+  advanceBy(context, rawContentLength + closeDelimiter.length)
 
   return {
     type: NodeTypes.INTERPOLATION,
